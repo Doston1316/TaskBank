47dddb534703473932789d03603a2eca7f66bed3
Doston1316 <84778369+Doston1316@users.noreply.github.com>
2021-09-20 15:22:44 +0500

    Initial commit

---
+++ src/main/java/com/example/taskbank/TaskBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskbank/TaskBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
